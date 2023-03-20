81b326d41087da03e68119072ca592ccc8dffc5b
Vinish M <vinishuchiha@gmail.com>
2023-03-20 16:06:21 +0530

    packaging files added

--- datacx/__init__.py
+++ datacx/__init__.py
@@ -1,1 +1,9 @@
 from .datacx import datacx
+from importlib import resources
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Version of the datacx package
+__version__ = "0.1.0"
