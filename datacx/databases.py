a50e20976e01881d85fbde363b27961270ec9cf9
Vinish M <vinishuchiha@gmail.com>
2023-03-22 12:27:30 +0530

    exception handling feature added

--- datacx/databases.py
+++ datacx/databases.py
@@ -1,4 +1,5 @@
 import connectorx as cx
+from exceptions import ParamsMissingException
 
 class Postgres():
     def __init__(self,config):
@@ -9,12 +10,15 @@ class Postgres():
                 self._pg_conn_str = f"{self._pg_conn_str}/{config['DATABASE']}"
 
     def read_as_dataframe(self,query,database=None,return_type='pandas'):
-        if self._dbname_in_config:
-            return cx.read_sql(self._pg_conn_str, query,return_type=return_type)
-        elif database:
-            return cx.read_sql(f"{self._pg_conn_str}/{database}", query,return_type=return_type)
-        else:
-            print('Missing Params: database name')
+        try:
+            if self._dbname_in_config:
+                return cx.read_sql(self._pg_conn_str, query,return_type=return_type)
+            elif database:
+                return cx.read_sql(f"{self._pg_conn_str}/{database}", query,return_type=return_type)
+            else:
+                raise ParamsMissingException(f"database parameter missing. Either add it in config or pass it as an argument.")
+        except ParamsMissingException as e:
+            print(e.message)
 
 class MySQL():
     def __init__(self,config):
@@ -25,9 +29,12 @@ class MySQL():
                 self._pg_conn_str = f"{self._pg_conn_str}/{config['DATABASE']}"
 
     def read_as_dataframe(self,query,database=None,return_type='pandas'):
-        if self._dbname_in_config:
-            return cx.read_sql(self._mysql_conn_str, query,return_type=return_type)
-        elif database:
-            return cx.read_sql(f"{self._mysql_conn_str}/{database}", query,return_type=return_type)
-        else:
-            print('Missing Params: database name')
+        try:
+            if self._dbname_in_config:
+                return cx.read_sql(self._mysql_conn_str, query,return_type=return_type)
+            elif database:
+                return cx.read_sql(f"{self._mysql_conn_str}/{database}", query,return_type=return_type)
+            else:
+                raise ParamsMissingException(f"database parameter missing. Either add it in config file or pass it as an argument.")
+        except ParamsMissingException as e:
+            print(e.message)
---
+++ datacx/exceptions.py
@@ -0,0 +1,3 @@
+class ParamsMissingException(Exception):
+    def __init__(self,message):
+        self.message = message
