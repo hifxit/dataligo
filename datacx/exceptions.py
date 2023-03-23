3f0f7aca33d2794499402c950b90f8f0b86cb489
Vinish M <vinishuchiha@gmail.com>
2023-03-23 15:36:02 +0530

    New exception added - ConfigMissingException

--- datacx/exceptions.py
+++ datacx/exceptions.py
@@ -1,3 +1,7 @@
 class ParamsMissingException(Exception):
     def __init__(self,message):
         self.message = message
+
+class ConfigMissingException(Exception):
+    def __init__(self,message):
+        self.message = message
