2f5fd36084af029bafea2aae6b46fff01c8d4036
Vinish M <vinishuchiha@gmail.com>
2023-04-03 15:20:11 +0530

    mariadb support removed temporarily because of C connector issue

--- datacx/databases/database.py
+++ datacx/databases/database.py
@@ -1,6 +1,6 @@
 import connectorx as cx
 import pandas as pd
-import mariadb
+#import mariadb
 from ..exceptions import ParamsMissingException
 from ..datawarehouses.utils import _df_to_file_writer
 
@@ -119,62 +119,62 @@ class Sqlite():
             return cx.read_sql(self._sqlite_conn, query, return_type=return_type)
 
 
-class MariaDB():
-    def __init__(self,config):
-        """
-        MariaDB class create dcx mariadb object, through which you can able to read, write, download data from MariaDB.
+# class MariaDB():
+#     def __init__(self,config):
+#         """
+#         MariaDB class create dcx mariadb object, through which you can able to read, write, download data from MariaDB.
 
-        Args:
-            config (dict): Automatically loaded from the config file (yaml)
-        """
-        self._config = config
-        if 'DATABASE' in config:
-            if config['DATABASE']:
-                self._dbname_in_config = True
-                self._mdb_conn = mariadb.connect(
-                    user=config['USERNAME'],
-                    password=config['PASSWORD'],
-                    host=config['HOST'],
-                    port=config['PORT'],
-                    database=config['DATABASE']
-                )
+#         Args:
+#             config (dict): Automatically loaded from the config file (yaml)
+#         """
+#         self._config = config
+#         if 'DATABASE' in config:
+#             if config['DATABASE']:
+#                 self._dbname_in_config = True
+#                 self._mdb_conn = mariadb.connect(
+#                     user=config['USERNAME'],
+#                     password=config['PASSWORD'],
+#                     host=config['HOST'],
+#                     port=config['PORT'],
+#                     database=config['DATABASE']
+#                 )
 
-    def read_as_dataframe(self,query: str,database: str=None, return_type='pandas'):
-        """_summary_
+#     def read_as_dataframe(self,query: str,database: str=None, return_type='pandas'):
+#         """_summary_
 
-        Args:
-            query (str): select query
-            database (str, optional): database name, if None, it take it from config. Defaults to None.
-            return_type (str, optional): which dataframe you want to return (pandas, polars, dask etc). Defaults to 'pandas'. Defaults to 'pandas'.
+#         Args:
+#             query (str): select query
+#             database (str, optional): database name, if None, it take it from config. Defaults to None.
+#             return_type (str, optional): which dataframe you want to return (pandas, polars, dask etc). Defaults to 'pandas'. Defaults to 'pandas'.
 
-        Returns:
-            DataFrame: Depends on the return_type parameter.
-        """
-        if self._dbname_in_config:
-            cur = self._mdb_conn.cursor()
-        elif database:
-            self._mdb_conn = mariadb.connect(
-                    user=self._config['USERNAME'],
-                    password=self._config['PASSWORD'],
-                    host=self._config['HOST'],
-                    port=self._config['PORT'],
-                    database=self._config['DATABASE']
-                )
-            cur = self._mdb_conn.cursor()
-        cur.execute(query)
-        records = [row for row in cur]
-        cur.close()
-        return pd.DataFrame(records)
+#         Returns:
+#             DataFrame: Depends on the return_type parameter.
+#         """
+#         if self._dbname_in_config:
+#             cur = self._mdb_conn.cursor()
+#         elif database:
+#             self._mdb_conn = mariadb.connect(
+#                     user=self._config['USERNAME'],
+#                     password=self._config['PASSWORD'],
+#                     host=self._config['HOST'],
+#                     port=self._config['PORT'],
+#                     database=self._config['DATABASE']
+#                 )
+#             cur = self._mdb_conn.cursor()
+#         cur.execute(query)
+#         records = [row for row in cur]
+#         cur.close()
+#         return pd.DataFrame(records)
     
-    def download_as_file(self,query: str, filename: str, database: str = None):
-        """
-        Takes query as argument and download the data as file
+#     def download_as_file(self,query: str, filename: str, database: str = None):
+#         """
+#         Takes query as argument and download the data as file
 
-        Args:
-            query (str): select query
-            filename (str): filename to save the file
-            database (str, optional): database name, if None, it take it from config. Defaults to None.
-        """
-        df = self.read_as_dataframe(query=query, database=database, return_type='pandas')
-        _df_to_file_writer(df, filename=filename)
-        print('File saved to the path:', filename)
+#         Args:
+#             query (str): select query
+#             filename (str): filename to save the file
+#             database (str, optional): database name, if None, it take it from config. Defaults to None.
+#         """
+#         df = self.read_as_dataframe(query=query, database=database, return_type='pandas')
+#         _df_to_file_writer(df, filename=filename)
+#         print('File saved to the path:', filename)
