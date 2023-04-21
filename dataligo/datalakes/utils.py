6f5d7074c0b8be242a72e764ec31df1ec54eceda
Vinish M <vinishuchiha@gmail.com>
2023-04-21 13:12:34 +0530

    write feature added to polars df to s3 and gcs

--- dataligo/datalakes/utils.py
+++ dataligo/datalakes/utils.py
@@ -6,6 +6,7 @@ from boto3.s3.transfer import TransferConfig
 import os
 import threading
 import sys
+from ..utils import which_dataframe
 
 multipart_config = TransferConfig(multipart_threshold=1024 * 50, 
                         max_concurrency=8,
@@ -92,47 +93,79 @@ def _s3_download_file(s3, s3_path=None, bucket=None, key=None, path_to_download=
     print('\n')
     print("File downloaded to the path:", f"{file_path}")
 
-def _s3_writer(s3, df, bucket, filename, extension, index=False, sep=','):
+def _s3_writer(s3, df, bucket, filename, extension, pandas_args = {}, polars_args = {}):
     buf = BytesIO()
     suffix = Path(filename).suffix
     if suffix:
         extension = suffix[1:]
     extension = extension.lower()
-    if extension=='csv':
-        df.to_csv(buf, index=index, sep=sep)
-    elif extension=='parquet':
-        df.to_parquet(buf, index=index)
-    elif extension=='json':
-        df.to_json(buf)
-    elif extension=='feather':
-        df.to_feather(buf)
-    elif extension in ['xlsx','xls']:
-        df.to_excel(buf, index=index)
-    else:
-        raise ExtensionNotSupportException(f'Unsupported Extension: {extension}')
+    if which_dataframe(df)=='pandas':
+        if extension=='csv':
+            df.to_csv(buf, **pandas_args)
+        elif extension=='parquet':
+            df.to_parquet(buf, **pandas_args)
+        elif extension=='json':
+            df.to_json(buf, **pandas_args)
+        elif extension=='feather':
+            df.to_feather(buf, **pandas_args)
+        elif extension in ['xlsx','xls']:
+            df.to_excel(buf, **pandas_args)
+        else:
+            raise ExtensionNotSupportException(f'Unsupported Extension: {extension}')
+    elif which_dataframe(df)=='polars':
+        if extension=='csv':
+            df.write_csv(buf, **polars_args)
+        elif extension=='parquet':
+            df.write_parquet(buf, **polars_args)
+        elif extension=='avro':
+            df.write_avro(buf, **polars_args)
+        elif extension=='json':
+            df.write_json(buf, **polars_args)
+        elif extension in ['feather','arrow']:
+            df.write_ipc(buf, **polars_args)
+        elif extension in ['xlsx','xls']:
+            df.write_excel(buf, **polars_args)
+        else:
+            raise ExtensionNotSupportException(f'Unsupported Extension: {extension}')
     buf.seek(0)
     s3.Bucket(bucket).put_object(Key=filename, Body=buf.getvalue())
     
-def _gcs_writer(gcs, df, bucket, filename, extension, index=False, sep=','):
+def _gcs_writer(gcs, df, bucket, filename, extension, pandas_args = {}, polars_args = {}):
     suffix = Path(filename).suffix
     if suffix:
         extension = suffix[1:]
     bucket = gcs.get_bucket(bucket)
     extension = extension.lower()
-    if extension=='csv':
-        bucket.blob(filename).upload_from_string(df.to_csv(index=index, sep=sep), 'text/csv')
-    elif extension=='parquet':
-        bucket.blob(filename).upload_from_string(df.to_parquet(index=index), 'text/parquet')
-    elif extension=='json':
-        bucket.blob(filename).upload_from_string(df.to_json(), 'text/json')
-    # elif extension=='feather':
-    #     bucket.blob(filename).upload_from_string(df.to_feather(), 'text/feather')
-    # elif extension in ['xlsx','xls']:
-    #     bucket.blob(filename).upload_from_string(df.to_excel(), 'text/xlsx')
-    else:
-        raise ExtensionNotSupportException(f'Unsupported Extension: {extension}')
+    if which_dataframe(df)=='pandas':
+        if extension=='csv':
+            bucket.blob(filename).upload_from_string(df.to_csv(**pandas_args), 'text/csv')
+        elif extension=='parquet':
+            bucket.blob(filename).upload_from_string(df.to_parquet(**pandas_args), 'text/parquet')
+        elif extension=='json':
+            bucket.blob(filename).upload_from_string(df.to_json(**pandas_args), 'text/json')
+        # elif extension=='feather':
+        #     bucket.blob(filename).upload_from_string(df.to_feather(), 'text/feather')
+        # elif extension in ['xlsx','xls']:
+        #     bucket.blob(filename).upload_from_string(df.to_excel(), 'text/xlsx')
+        else:
+            raise ExtensionNotSupportException(f'Unsupported Extension: {extension}')
+    elif which_dataframe(df)=='polars':
+        if extension=='csv':
+            bucket.blob(filename).upload_from_string(df.write_csv(**polars_args), 'text/csv')
+        # elif extension=='parquet':
+        #     bucket.blob(filename).upload_from_string(df.write_parquet(**polars_args), 'text/parquet')
+        elif extension=='json':
+            bucket.blob(filename).upload_from_string(df.write_json(**polars_args), 'text/json')
+        # elif extension=='avro':
+        #     bucket.blob(filename).upload_from_string(df.write_avro(**polars_args), 'text/avro')
+        # elif extension in ['feather','arrow']:
+        #     df.write_ipc(buf, **polars_args)
+        elif extension in ['xlsx','xls']:
+            bucket.blob(filename).upload_from_string(df.write_avro(**polars_args), 'text/avro')
+        else:
+            raise ExtensionNotSupportException(f'Unsupported Extension: {extension}')
 
-def _azure_blob_writer(abs, df, container_name,blob_name, extension, overwrite=True, index=False, sep=','):
+def _azure_blob_writer(abs, df, container_name,blob_name, extension, overwrite=True, pandas_args = {}, polars_args = {}):
     suffix = Path(blob_name).suffix
     if suffix:
         extension = suffix[1:]
@@ -140,15 +173,15 @@ def _azure_blob_writer(abs, df, container_name,blob_name, extension, overwrite=T
     blob_client = container_client.get_blob_client(blob_name)
     buf = BytesIO()
     if extension=='csv':
-        df.to_csv(buf,encoding='utf-8',index=index,sep=sep)
+        df.to_csv(buf, **pandas_args)
     elif extension=='json':
-        df.to_json(buf)
+        df.to_json(buf, **pandas_args)
     elif extension=='parquet':
-        df.to_parquet(buf)
+        df.to_parquet(buf, **pandas_args)
     elif extension=='feather':
-        df.to_feather(buf)
+        df.to_feather(buf, **pandas_args)
     elif extension in ['xlsx','xls']:
-        df.to_excel(buf)
+        df.to_excel(buf, **pandas_args)
     else:
         raise ExtensionNotSupportException(f'Unsupported Extension: {extension}')
     buf.seek(0)
